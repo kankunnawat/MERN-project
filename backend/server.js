6c2491cc26982f0eccf8be10140f303d2d0cc70b
kan1657 <kan1657@hotmail.com>
2022-02-12 23:30:08 +0700

    Add Routes & Controller

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,12 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Support Desk API' })
 })
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.listen(PORT, () => console.log('Server started on port ' + PORT))
