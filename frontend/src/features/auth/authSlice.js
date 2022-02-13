37b3099d6bff80ca70fd16b5db80d2faeb11277c
kan1657 <kan1657@hotmail.com>
2022-02-13 14:25:37 +0700

    Hook Register Form To Redux

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -7,6 +7,17 @@ const initialState = {
   isLoading: false,
   message: '',
 }
+// Register new user
+export const register = createAsyncThunk(
+  'auth/register',
+  async (user, thunkAPI) => {
+    console.log(user)
+  }
+)
+// Login user
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
+  console.log(user)
+})
 
 export const authSlice = createSlice({
   name: 'auth',
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import { FaUser } from 'react-icons/fa'
 import { toast } from 'react-toastify'
+import { useSelector, useDispatch } from 'react-redux'
+import { register } from '../features/auth/authSlice'
+
 function Register() {
   const [formData, setFormData] = useState({
     name: '',
@@ -10,6 +13,12 @@ function Register() {
   })
 
   const { name, email, password, password2 } = formData
+
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -23,6 +32,13 @@ function Register() {
 
     if (password !== password2) {
       toast.error('Password do not match')
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      }
+      dispatch(register(userData))
     }
   }
 
@@ -31,7 +47,7 @@ function Register() {
       <section className="heading">
         <h1>
           <FaUser />
-          Register
+          Register {user}
         </h1>
         <p>Please create an account</p>
       </section>
@@ -75,7 +91,7 @@ function Register() {
           </div>
           <div className="form-group">
             <input
-              type="password2"
+              type="password"
               className="form-control"
               id="password2"
               name="password2"
