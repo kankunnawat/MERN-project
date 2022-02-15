ce382c3d52e1536c973ca0bde7bb3f3b395de686
kan1657 <kan1657@hotmail.com>
2022-02-15 13:37:02 +0700

    fix bug private route can't get loggedIn

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,11 +1,13 @@
 import { Navigate, Outlet } from 'react-router-dom'
-import { useAuthStatus } from '../hook/useAuthStatus'
+import { useAuthStatus } from '../hooks/useAuthStatus'
 import Spinner from './Spinner'
 
 const PrivateRoute = () => {
-  const { loggedIn, checkingStatus } = useAuthStatus
+  const { loggedIn, checkingStatus } = useAuthStatus()
 
-  if (checkingStatus) return <Spinner />
+  if (checkingStatus) {
+    return <Spinner />
+  }
 
   return loggedIn ? <Outlet /> : <Navigate to="/login" />
 }
